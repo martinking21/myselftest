32f3d3f6ef1ae3d2791746f47ee944c794c7c304
martinking21 <martinkingkabezya9@gmail.com>
2024-11-18 14:13:06 +0100

    change version vv

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Item(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
     description = models.TextField()
 
     def __str__(self):
